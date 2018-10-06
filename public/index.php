4525b658273b1bf51c728104f776883fa9f742ec
Surabhi Chaudhari <sc2263@njit.edu>
2018-10-06 01:14:42 -0400

    Feature: Creation of field names and addition of key and value

--- public/index.php
+++ public/index.php
@@ -24,10 +24,23 @@ class csv {
 
         $file = fopen($filename, "r");
 
+        $fieldNames = array();
+        $count = 0;
+
         while(! feof($file)){
 
             $record = fgetcsv($file);
-            $records[] = recordFactory::create($record);
+
+            if($count==0){
+
+                $fieldNames = $record;
+            }
+
+            else{
+
+                $records[] = recordFactory::create($fieldNames,$record);
+            }
+            $count++;
         }
 
         fclose($file);
@@ -56,14 +69,17 @@ class record{
 class recordFactory
 {
 
-    public static function create(Array $array = null)
+    public static function create(Array $fieldNames = null, Array $record = null)
     {
 
-
-        $record = new record($array);
+        print_r($fieldNames);
+        print_r($record);
+        // $record = new record($array);
 
         return $record;
 
     }
 }
+
+
 
